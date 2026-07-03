11dc5131274e6e465721047e04c693f851f00bdc
sergioahp <sergioahp@proton.me>
2026-07-03 04:10:16 -0600

    Add smoll code for 🗺⛺

---
+++ Proyecto/tent-map-fig/main.py
@@ -0,0 +1,29 @@
+import torch
+import einops
+import matplotlib.pyplot as plt
+torch.set_default_dtype(torch.float64)
+
+# Tent-map: mu min(x, 1-x)
+
+# We wish to the bifurcation diagram, i.e. the long term values of x_n for each
+# mu in [0,2]
+
+mu_vals = torch.linspace(1, 2, 4000)
+x_vals = torch.linspace(0, 1, 1000)
+x, mu = torch.meshgrid(x_vals, mu_vals, indexing='ij')
+
+n_iter = 1000
+
+for i in range(n_iter):
+    x = mu * torch.min(x, 1 - x)
+
+
+
+
+plt.figure(figsize=(10, 7))
+plt.xlim(1, 2)
+plt.ylim(0, 1)
+plt.xlabel(r'$\mu$')
+plt.ylabel(r'$x$')
+plt.scatter(mu, x, s=0.25, alpha=0.4, color='black', marker='.', linewidths=0)
+plt.savefig('tent_map.png', dpi=600)

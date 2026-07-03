2ec26b910494db8dbd469b991f9ff4d441637c23
sergioahp <sergioahp@proton.me>
2026-07-03 04:21:17 -0600

    Add title, remove stray image

--- Proyecto/tent-map-fig/main.py
+++ Proyecto/tent-map-fig/main.py
@@ -13,6 +13,7 @@ x_vals = torch.linspace(0, 1, 1000)
 x, mu = torch.meshgrid(x_vals, mu_vals, indexing='ij')
 
 n_iter = 1000
+plot_title = "Tent Map Bifurcation Diagram"
 
 for i in range(n_iter):
     x = mu * torch.min(x, 1 - x)
@@ -23,7 +24,9 @@ for i in range(n_iter):
 plt.figure(figsize=(10, 7))
 plt.xlim(1, 2)
 plt.ylim(0, 1)
+plt.title(plot_title)
 plt.xlabel(r'$\mu$')
 plt.ylabel(r'$x$')
 plt.scatter(mu, x, s=0.25, alpha=0.4, color='black', marker='.', linewidths=0)
-plt.savefig('tent_map.png', dpi=600)
+plt.tight_layout()
+plt.savefig('tent_map.png', dpi=600, bbox_inches='tight')
